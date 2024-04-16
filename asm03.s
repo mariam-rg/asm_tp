1bd766a1169c2de999b3b5d33663b080fc646e2a
mariam-rg <mariamregragui2002@gmail.com>
2024-04-16 09:06:22 +0200

    modification de asm03

--- asm03.s
+++ asm03.s
@@ -1,62 +1,55 @@
 section .data
     msg db '1337', 10
-    msg_true db 'O', 10
-    msg_false db '1', 10
+    message_len equ $ - message
+
+section .bss
+    input_buffer resb 16
+
+
+global _start
 
 section .text
-    global _start
 
 _start:
+
+    mov rdi, 0          
+    mov rax, 0          
+    mov rsi, input_buffer 
+    mov rdx, 16         
+    syscall           
     
-    cmp qword [rsp + 8], 2
-    jne error
+
+    mov rax, 1          
+    mov rdi, 1          
+    mov rsi, msg        
+    mov rdx, 5          
+    syscall
+
+   
+    mov rax, 1         
+    mov rdi, 1          
+    mov rsi, input      
+    mov rdx, 255         
+    syscall
 
     
-    mov rdi, [rsp + 16]
-    
-    
-    cmp rdi, 42
+    cmp byte [input], '4'
+    cmp byte [input+1], '2'
+    je equal
     jne not_equal
 
     
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 4
-    syscall
-
     
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg_true
-    mov rdx, 2
-    syscall
-    jmp end
 
 not_equal:
     
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg_false
-    mov rdx, 2
+    mov rax, 60          
+    mov rdi, 1                     
     syscall
 
-end:
+equal:
     
     mov rax, 60         
-    xor rdi, rdi        
-    syscall
-
-error:
-    
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, error_msg
-    mov rdx, error_len
-    syscall
-
-    
-    mov rax, 60         
-    mov rdi, 1          
+    mov rdi, 0      
     syscall
 
