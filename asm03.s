6266489a9f1896d37c07cefbb09e75c86dcbfc9f
mariam-rg <mariamregragui2002@gmail.com>
2024-04-16 11:14:48 +0200

    modifier

--- asm03.s
+++ asm03.s
@@ -1,55 +1,43 @@
 section .data
     msg db '1337', 10
-    message_len equ $ - message
-
-section .bss
-    input_buffer resb 16
-
 
 global _start
 
 section .text
-
 _start:
+    pop rax
+    pop rax
+    pop rax
 
-    mov rdi, 0          
-    mov rax, 0          
-    mov rsi, input_buffer 
-    mov rdx, 16         
-    syscall           
-    
+    cmp byte [rax], '4'
+    jne _error
+    je _affiche
 
+    cmp byte [rax+1], '2'
+    jne _error
+    je _affiche
+
+    cmp byte [rax+2], '0'
+    jne _error
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+
+_affiche:
     mov rax, 1          
     mov rdi, 1          
     mov rsi, msg        
     mov rdx, 5          
     syscall
 
-   
-    mov rax, 1         
-    mov rdi, 1          
-    mov rsi, input      
-    mov rdx, 255         
+    mov rax, 60
+    mov rdi, 0
     syscall
 
-    
-    cmp byte [input], '4'
-    cmp byte [input+1], '2'
-    je equal
-    jne not_equal
 
-    
-    
-
-not_equal:
-    
-    mov rax, 60          
-    mov rdi, 1                     
+_error:
+    mov rax, 60
+    mov rdi, 1
     syscall
-
-equal:
-    
-    mov rax, 60         
-    mov rdi, 0      
-    syscall
-
