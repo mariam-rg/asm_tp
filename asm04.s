fdfad391ef216534ccc817c1ad4a553cbb7e2af6
mariam-rg <mariamregragui2002@gmail.com>
2024-04-18 10:09:49 +0200

    Modifier am04

--- asm04.s
+++ asm04.s
@@ -1,49 +1,76 @@
 section .data
-    input db 0
-    output db 0
-
-section .text
-    global _start
-
-_start:
-    ; Affiche le message pour demander à l'utilisateur d'entrer un nombre
-    mov rax, 0          
-    mov rdi, 1          
-    mov rsi, message    
-    mov rdx, message_len 
-    syscall
-
-    ; Lit l'entrée utilisateur
-    mov rax, 0          
-    mov rdi, 0          
-    mov rsi, input      
-    mov rdx, 1          
-    syscall
-
-    ; Convertit le caractère en nombre
-    sub byte [input], '0'
-
-    ; Vérifie si le nombre est pair ou impair
-    test byte [input], 1 
-    jnz .odd             
-    mov byte [output], '0' 
-    jmp .end_prog
-.odd:
-    mov byte [output], '1' 
-.end_prog:
-
-    
-    mov rax, 0          
-    mov rdi, 1          
-    mov rsi, output     
-    mov rdx, 1          
-    syscall
-
-    
-    mov rax, 60         
-    xor rdi, rdi        
-    syscall
+   
+   
+global _start
 
 section .bss
-    message resb 30     
-    message_len equ $ - message
+    input resb 64
+
+section .text
+
+_start:
+    mov rax, 0      
+    mov rdi, 0    
+    mov rsi, input      
+    mov rdx, 64      
+    syscall
+
+
+    mov r8, rsi
+    call _conv
+
+    mov rbx, 2
+    xor rdx, rdx
+    div rbx
+    cmp rdx, 0
+    je _pair
+    jne _notpair
+
+; rcx, rdx, rax, r8, rbx
+_conv:
+    xor rax,rax
+    xor rcx, rcx
+    xor dl, dl
+    xor rdx, rdx
+
+_conv_loop:
+
+    mov dl, [r8+rcx]
+
+    cmp dl, byte 0
+    je _conv_end
+    cmp dl, 10
+    je _conv_end
+
+    sub dl, 48
+    add al, dl
+
+    cmp [r8+rcx+1], byte 0
+    je _conv_end
+    cmp [r8+rcx+1], byte 10
+    je _conv_end
+
+    mov bl, 10
+    mul bl
+    inc rcx
+    jmp _conv_loop
+
+_conv_end:
+    ret
+
+ 
+
+_pair:
+    mov rax, 60          
+    mov rdi, 0                    
+    syscall
+
+
+
+   
+_notpair:
+    mov rax, 60          
+    mov rdi, 1                    
+    syscall
+
+   
