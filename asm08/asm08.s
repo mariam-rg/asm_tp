5d38ddfdca9f5e513cb6f442e7a667e32b96ab1d
mariam-rg <mariamregragui2002@gmail.com>
2025-02-07 00:44:41 +0100

    modif

--- asm08/asm08.s
+++ asm08/asm08.s
@@ -1,142 +1,104 @@
-sys_stdin   equ 0
-sys_stdout  equ 1
-
-sys_read    equ 0
-sys_write   equ 1
-sys_exit    equ 60
-
 section .data
-    txt db " "
+    newline db 10
+
+section .bss
+    buffer resb 32    ; Buffer for output string
 
 section .text
-    global _start
+global _start
 
 _start:
-    pop r8
+    ; Check if we have exactly one argument
+    pop rax         ; Get argc
+    cmp rax, 2      ; We need exactly 2 arguments (program name + number)
+    jne error
 
+    ; Get the argument
+    pop rax         ; Skip program name
+    pop rdi         ; Get number argument
+    call atoi       ; Convert string to integer
 
-    cmp r8, 2
-    jnz .error
+    ; Calculate sum of numbers from 1 to n-1
+    dec rax         ; Subtract 1 to get upper limit
+    call calculate_sum
 
-    pop rdi
-    pop rdi
+    ; Convert result to string and print
+    mov rdi, buffer
+    call itoa
 
-    call atoi
-
-
-    cmp rax, -1
-    jnz .error
-
-    mov r9, rax
-
-.convert_to_binary:
-    mov rbx, 2
-
-.convert_to_binary_loop:
-    cmp rax, 1
-    jl .display_binary
-
-
-    div rbx
-    add rdx, byte 48
-
-    push rdx
-    xor rdx, rdx
-
-    jmp .convert_to_binary_loop
-
-.display_binary:
-    pop rdx
-
-    test rdx,rdx
-    jz .display_binary_end
-
-    mov rax, sys_write
-    mov rdi, sys_stdout
-    mov [txt], rdx
-    mov rsi, txt
-    mov rdx, 1
+    ; Print the result
+    mov rax, 1      ; sys_write
+    mov rdi, 1      ; stdout
+    mov rsi, buffer
+    mov rdx, rbx    ; Length of string (set by itoa)
     syscall
 
-    jmp .display_binary
-
-.display_binary_end:
-
-    mov rax, sys_write
-    mov rdi, sys_stdout
-    mov [txt], byte 10
-    mov rsi, txt
-    mov rdx, 1
-    syscall
-
-
-    jmp .exit_success
-
-.error:
-    mov rax, sys_exit
+    ; Print newline
+    mov rax, 1
     mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
     syscall
 
-.exit_success:
-    mov rax, sys_exit
-    mov rdi, r10
-    syscall
+    jmp exit_success
 
+; Function to calculate sum: (n * (n + 1)) / 2
+calculate_sum:
+    mov rbx, rax    ; Save n
+    inc rax         ; n + 1
+    mul rbx         ; n * (n + 1)
+    shr rax, 1      ; Divide by 2
+    ret
+
+; Function to convert string to integer (atoi)
 atoi:
-    xor rsi, rsi
-    call strlen
-    mov rcx, rax
-    xor rax, rax
-
-.atoi_loop:
-
-    cmp   rsi, rcx
-    jge   .atoi_end
-
-
-    mov   dl, byte [rdi + rsi]
-
-
-    cmp dl, 10
-    jz .atoi_end
-
-
-    cmp dl, '0'
-    jl .atoi_err
-
-    cmp dl, '9'
-    jg .atoi_err
-
-    add   rax, rax
-    lea   rax, [4 * rax + rax]
-
-    sub   dl, "0"
-    movzx rdx, dl
-    add   rax, rdx
-
-.atoi_inc:
-    inc   rsi
-    jmp   .atoi_loop
-
-.atoi_err:
-    mov rax, -1
+    xor rax, rax    ; Clear result
+atoi_loop:
+    movzx rcx, byte [rdi] ; Get next character
+    test rcx, rcx   ; Check for null terminator
+    jz atoi_done
+    sub rcx, '0'    ; Convert ASCII to number
+    jb error        ; Jump if below 0
+    cmp rcx, 9
+    ja error        ; Jump if above 9
+    imul rax, 10    ; Multiply current value by 10
+    add rax, rcx    ; Add new digit
+    inc rdi         ; Move to next character
+    jmp atoi_loop
+atoi_done:
     ret
 
-.atoi_end:
-    xor rdx, rdx
+; Function to convert integer to string (itoa)
+itoa:
+    mov rax, rbx    ; Number to convert
+    mov rcx, 0      ; Character counter
+    mov rbx, 10     ; Divisor
+
+itoa_loop:
+    xor rdx, rdx    ; Clear rdx for division
+    div rbx         ; Divide by 10
+    add dl, '0'     ; Convert remainder to ASCII
+    dec rdi         ; Move buffer pointer back
+    mov [rdi], dl   ; Store digit
+    inc rcx         ; Increment counter
+    test rax, rax   ; Check if we're done
+    jnz itoa_loop
+
+    ; Move string to beginning of buffer
+    mov rax, buffer
+    mov rbx, rcx    ; Save length for printing
+    mov rsi, rdi    ; Source: current position
+    mov rdi, rax    ; Destination: start of buffer
+    rep movsb       ; Move string
+
     ret
 
+exit_success:
+    mov rax, 60     ; sys_exit
+    xor rdi, rdi    ; return 0
+    syscall
 
-strlen:
-    xor rax, rax
-
-.strlen_loop:
-
-    cmp [rdi + rax], byte 0
-    jz .strlen_end
-
-    inc rax
-    jmp .strlen_loop
-
-.strlen_end:
-    ret
+error:
+    mov rax, 60     ; sys_exit
+    mov rdi, 1      ; return 1
+    syscall
