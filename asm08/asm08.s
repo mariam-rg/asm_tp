fc0e25873cb68e48f46edbc1022d7f13577fcd47
mariam-rg <mariamregragui2002@gmail.com>
2025-02-07 00:47:33 +0100

    modif

--- asm08/asm08.s
+++ asm08/asm08.s
@@ -17,80 +17,105 @@ _start:
     pop rax         ; Skip program name
     pop rdi         ; Get number argument
     call atoi       ; Convert string to integer
-
+    
+    ; Check if number is less than 0
+    cmp rax, 0
+    jl error
+    
+    ; Save the original number for later
+    mov rbx, rax
+    
     ; Calculate sum of numbers from 1 to n-1
     dec rax         ; Subtract 1 to get upper limit
-    call calculate_sum
+    cmp rax, 0      ; Check if result is negative
+    jl zero_result
+    
+    ; Calculate sum using: (n * (n + 1)) / 2
+    mov rcx, rax    ; Save n
+    inc rax         ; n + 1
+    mul rcx         ; n * (n + 1)
+    shr rax, 1      ; Divide by 2
+    jmp print_result
 
-    ; Convert result to string and print
+zero_result:
+    xor rax, rax    ; Return 0 for input of 0 or 1
+
+print_result:
+    ; Convert result to string
     mov rdi, buffer
     call itoa
-
+    
     ; Print the result
     mov rax, 1      ; sys_write
     mov rdi, 1      ; stdout
     mov rsi, buffer
-    mov rdx, rbx    ; Length of string (set by itoa)
+    mov rdx, rcx    ; Length of string (set by itoa)
     syscall
-
+    
     ; Print newline
     mov rax, 1
     mov rdi, 1
     mov rsi, newline
     mov rdx, 1
     syscall
-
+    
     jmp exit_success
-
-; Function to calculate sum: (n * (n + 1)) / 2
-calculate_sum:
-    mov rbx, rax    ; Save n
-    inc rax         ; n + 1
-    mul rbx         ; n * (n + 1)
-    shr rax, 1      ; Divide by 2
-    ret
 
 ; Function to convert string to integer (atoi)
 atoi:
     xor rax, rax    ; Clear result
+    xor rcx, rcx    ; Clear counter
 atoi_loop:
-    movzx rcx, byte [rdi] ; Get next character
-    test rcx, rcx   ; Check for null terminator
+    movzx rdx, byte [rdi + rcx] ; Get next character
+    test dl, dl     ; Check for null terminator
     jz atoi_done
-    sub rcx, '0'    ; Convert ASCII to number
-    jb error        ; Jump if below 0
-    cmp rcx, 9
-    ja error        ; Jump if above 9
+    cmp dl, '0'     ; Check if less than '0'
+    jb error
+    cmp dl, '9'     ; Check if greater than '9'
+    ja error
+    sub dl, '0'     ; Convert ASCII to number
     imul rax, 10    ; Multiply current value by 10
-    add rax, rcx    ; Add new digit
-    inc rdi         ; Move to next character
+    add rax, rdx    ; Add new digit
+    inc rcx         ; Move to next character
     jmp atoi_loop
 atoi_done:
+    test rcx, rcx   ; Check if we converted any digits
+    jz error        ; If not, error
     ret
 
 ; Function to convert integer to string (itoa)
 itoa:
-    mov rax, rbx    ; Number to convert
-    mov rcx, 0      ; Character counter
-    mov rbx, 10     ; Divisor
+    mov rsi, buffer     ; End of buffer
+    add rsi, 31         ; Point to last character
+    mov byte [rsi], 0   ; Null terminate
+    mov rcx, 0          ; Character counter
+    
+    ; Handle 0 specially
+    test rax, rax
+    jnz itoa_loop
+    dec rsi
+    mov byte [rsi], '0'
+    inc rcx
+    jmp itoa_done
 
 itoa_loop:
-    xor rdx, rdx    ; Clear rdx for division
-    div rbx         ; Divide by 10
-    add dl, '0'     ; Convert remainder to ASCII
-    dec rdi         ; Move buffer pointer back
-    mov [rdi], dl   ; Store digit
-    inc rcx         ; Increment counter
-    test rax, rax   ; Check if we're done
-    jnz itoa_loop
+    test rax, rax
+    jz itoa_done
+    
+    xor rdx, rdx
+    mov rbx, 10
+    div rbx             ; Divide by 10
+    add dl, '0'         ; Convert remainder to ASCII
+    dec rsi
+    mov [rsi], dl
+    inc rcx
+    jmp itoa_loop
 
-    ; Move string to beginning of buffer
-    mov rax, buffer
-    mov rbx, rcx    ; Save length for printing
-    mov rsi, rdi    ; Source: current position
-    mov rdi, rax    ; Destination: start of buffer
-    rep movsb       ; Move string
-
+itoa_done:
+    mov rdi, buffer     ; Start of buffer
+    push rcx            ; Save length
+    rep movsb           ; Move string to start of buffer
+    pop rcx             ; Restore length
     ret
 
 exit_success:
