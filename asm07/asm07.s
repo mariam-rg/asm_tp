367b55162154b9a3e1575e8d064b5e4af22b95af
mariam-rg <mariamregragui2002@gmail.com>
2025-02-07 15:25:43 +0100

    modif

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -5,15 +5,15 @@ section .text
     global _start
 
 _start:
-    pop r8
-    cmp r8, 2
+    pop r8         ; Get argc
+    dec r8         ; Decrease by 1 to get actual argument count
+    cmp r8, 1      ; Check if we have exactly 1 argument
     jne _error_input
 
-    pop rdi
-    pop rdi
+    pop rdi        ; Program name
+    pop rdi        ; Argument
 
     call convert_to_dec
-
     cmp rax, -1
     je _error_input
 
@@ -27,59 +27,49 @@ _check_prime:
     mov rax, r9
     xor rdx, rdx
     div rcx
-
+    
     cmp rdx, 0     ; Check if divisible
-    je _print_and_exit_nonprime
-
+    je _not_prime
+    
     inc rcx
     mov rax, rcx
     mul rax        ; rcx * rcx
     cmp rax, r9    ; Compare with original number
     jle _check_prime
 
+_is_prime:
     mov rax, r9
-    jmp _print_and_exit_prime
-
-_print_and_exit_prime:
     call _print_number
     mov rax, 60
-    xor rdi, rdi   ; Exit code 0 for prime
-    syscall
-
-_print_and_exit_nonprime:
-    call _print_number
-    mov rax, 60
-    mov rdi, 1     ; Exit code 1 for non-prime
+    xor rdi, rdi   ; Exit 0 for prime
     syscall
 
 _not_prime:
+    mov rax, r9
     call _print_number
     mov rax, 60
-    mov rdi, 1     ; Exit code 1 for non-prime
+    mov rdi, 1     ; Exit 1 for non-prime
     syscall
 
 _print_number:
-    xor rcx, rcx   ; Counter for digits
+    push rax       ; Save number
+    xor rcx, rcx
 
 _push_number_loop:
     xor rdx, rdx
     mov rbx, 10
     div rbx
-
     add rdx, '0'
     push rdx
-    inc rcx        ; Track number of digits
-
+    inc rcx
     test rax, rax
     jnz _push_number_loop
 
 _display_numbers:
     test rcx, rcx
     jz _print_number_end
-
     pop rdx
     mov [txt], dl
-
     push rcx
     mov rax, 1
     mov rdi, 1
@@ -87,22 +77,22 @@ _display_numbers:
     mov rdx, 1
     syscall
     pop rcx
-
     dec rcx
     jmp _display_numbers
 
 _print_number_end:
-    mov byte [txt], 10    ; Newline
+    mov byte [txt], 10
     mov rax, 1
     mov rdi, 1
     mov rsi, txt
     mov rdx, 1
     syscall
+    pop rax        ; Restore number
     ret
 
 _error_input:
     mov rax, 60
-    mov rdi, 2     ; Exit code 2 for bad input
+    mov rdi, 2     ; Exit 2 for bad input
     syscall
 
 convert_to_dec:
@@ -114,25 +104,18 @@ convert_to_dec:
 _convert_to_dec_loop:
     cmp rsi, rcx
     jge _convert_to_dec_end
-
     mov dl, byte [rdi + rsi]
-
     cmp dl, 10
     jz _convert_to_dec_end
-
     cmp dl, '0'
     jl _convert_to_dec_err
-
     cmp dl, '9'
     jg _convert_to_dec_err
-
     add rax, rax
     lea rax, [4 * rax + rax]
-
     sub dl, '0'
     movzx rdx, dl
     add rax, rdx
-
     inc rsi
     jmp _convert_to_dec_loop
 
@@ -145,12 +128,10 @@ _convert_to_dec_end:
 
 string_len:
     xor rax, rax
-
 _string_len_loop:
     cmp byte [rdi + rax], 0
     jz _string_len_end
     inc rax
     jmp _string_len_loop
-
 _string_len_end:
     ret
