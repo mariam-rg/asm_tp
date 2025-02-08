2b99807ef74ebb2579bc1d365372c234a6a46c0e
mariam-rg <mariamregragui2002@gmail.com>
2025-02-08 16:52:11 +0100

    Update

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -1,75 +1,150 @@
 section .data
-    buffer db 20 dup(0)
+    txt db " "
 
 section .text
     global _start
 
 _start:
-    ; Read input
-    mov rax, 0          ; sys_read
-    mov rdi, 0          ; stdin
-    mov rsi, buffer     ; buffer
-    mov rdx, 20         ; size
+    pop r8
+    cmp r8, 2
+    jnz _error
+
+    pop rdi
+    pop rdi
+
+    call convert_to_dec
+
+
+    cmp rax, -1
+    jz _error
+
+    cmp rax, 0
+    jl _error
+
+    cmp rax, 0
+    jz _print_0
+
+    dec rax
+    mov rcx, rax
+    dec rcx
+
+_loop:
+    cmp rcx, 1
+    jl _print_number
+
+    add rax, rcx
+    dec rcx
+
+    jmp _loop
+
+_print_0:
+    mov rax, 0
+
+_print_number:
+
+_push_number_loop:
+    xor rdx, rdx
+    mov rbx, 10
+    div rbx
+
+    add rdx, '0'
+    push rdx
+
+    cmp rax, 1
+    jl _display_numbers
+
+    jmp _push_number_loop
+
+_display_numbers:
+    pop rdx
+
+    test rdx, rdx
+    jz _print_number_end
+
+
+    mov rax, 1
+    mov rdi, 1
+    mov [txt], rdx
+    mov rsi, txt
+    mov rdx, 1
     syscall
 
-    ; Convert string to number
-    xor rax, rax        ; Clear rax
-    mov rcx, buffer     ; Point to buffer
-_convert:
-    movzx rdx, byte [rcx]
-    cmp dl, 10          ; Check for newline
-    je _check_prime
-    cmp dl, '0'
-    jl _error
-    cmp dl, '9'
-    jg _error
+    jmp _display_numbers
 
-    sub dl, '0'         ; Convert to number
-    imul rax, 10        ; Multiply by 10
-    add rax, rdx        ; Add digit
-    inc rcx
-    jmp _convert
+_print_number_end:
 
-_check_prime:
-    cmp rax, 1          ; 1 is not prime
-    jle _not_prime
 
-    mov rcx, 2          ; Start checking from 2
-_loop:
-    mov rdx, 0
-    push rax
-    div rcx
-    pop rax
-    cmp rdx, 0          ; Check remainder
-    je _check_if_same
+    mov rax, 1
+    mov rdi, 1
+    mov [txt], byte 10
+    mov rsi, txt
+    mov rdx, 1
+    syscall
 
-    inc rcx
-    push rax
-    mov rdx, 0
-    mov rax, rcx
-    mul rcx             ; rcx * rcx
-    cmp rax, qword [rsp]
-    pop rax
-    jle _loop
-    jmp _is_prime
+    xor rcx, rcx
 
-_check_if_same:
-    cmp rcx, rax        ; If n/2 == n, it's prime
-    je _is_prime
-    jmp _not_prime
-
-_is_prime:
-    mov rdi, 0          ; Return 0
-    jmp _exit
-
-_not_prime:
-    mov rdi, 1          ; Return 1
-    jmp _exit
+_success:
+    mov rax, 60
+    mov rdi, 0
+    syscall
 
 _error:
-    mov rax, 60         ; sys_exit
-    mov rdi, 2          ; return 2 for invalid input
-
-_exit:
-    mov rax, 60         ; sys_exit
+    mov rax, 60
+    mov rdi, 1
     syscall
+
+convert_to_dec:
+    xor rsi, rsi
+    call string_len
+    mov rcx, rax
+    xor rax, rax
+
+_convert_to_dec_loop:
+
+    cmp   rsi, rcx
+    jge   _convert_to_dec_end
+
+    mov   dl, byte [rdi + rsi]
+
+    cmp dl, 10
+    jz _convert_to_dec_end
+
+    cmp dl, '0'
+    jl _convert_to_dec_err
+
+    cmp dl, '9'
+    jg _convert_to_dec_err
+
+    add   rax, rax
+    lea   rax, [4 * rax + rax]
+
+    sub   dl, "0"
+    movzx rdx, dl
+    add   rax, rdx
+
+_convert_to_dec_inc:
+    inc   rsi
+    jmp   _convert_to_dec_loop
+
+_convert_to_dec_err:
+    mov rax, -1
+    ret
+
+_convert_to_dec_end:
+    xor rdx, rdx
+    ret
+
+
+string_len:
+    xor rax, rax
+
+_string_len_loop:
+
+    cmp [rdi + rax], byte 0
+    jz _string_len_end
+
+    inc rax
+    jmp _string_len_loop
+
+_string_len_end:
+    ret
