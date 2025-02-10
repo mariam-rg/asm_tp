2ce2d7e09cfd090812af940a332b224d56cc85d5
mariam-rg <mariamregragui2002@gmail.com>
2025-02-10 19:39:08 +0100

    update

--- asm10/asm10.s
+++ asm10/asm10.s
@@ -1,74 +1,115 @@
+section .data
+    newline db 10
+
+section .bss
+    buffer resb 32
+
 section .text
-    global _start
+global _start
 
 _start:
-    pop r8          ; Get argc
-    cmp r8, 4       ; Need exactly 3 parameters (prog_name + 3 args)
-    jne _error
+    pop rcx         ; Get argc
+    cmp rcx, 4      ; Need exactly 4 arguments
+    jne error
 
-    pop rdi         ; Skip program name
-    pop rdi         ; Get first number string
-    call _atoi
-    mov r9, rax     ; Store first number in r9
+    pop rcx         ; Skip program name
+    pop rdi         ; First number
+    call atoi
+    mov r12, rax    ; Store first number
 
-    pop rdi         ; Get second number string
-    call _atoi
-    cmp rax, r9     ; Compare with first number
-    jne _not_same
+    pop rdi         ; Second number
+    call atoi
+    mov r13, rax    ; Store second number
 
-    pop rdi         ; Get third number string
-    call _atoi
-    cmp rax, r9     ; Compare with first number
-    jne _not_same
+    pop rdi         ; Third number
+    call atoi
+    mov r14, rax    ; Store third number
 
-    jmp _all_same   ; If we get here, all numbers are same
+    ; Find maximum
+    mov rax, r12    ; Start with first number
+    cmp rax, r13    ; Compare with second
+    jge check_third
+    mov rax, r13    ; Second is larger
 
-_atoi:
-    xor rax, rax        ; Clear result
-    xor rcx, rcx        ; Clear index
-    mov r10b, 0         ; Clear negative flag
-    
-    ; Check for negative sign
-    cmp byte [rdi], '-'
-    jne .loop
-    inc rdi             ; Skip minus sign
-    mov r10b, 1         ; Set negative flag
+check_third:
+    cmp rax, r14    ; Compare with third
+    jge print_result
+    mov rax, r14    ; Third is larger
 
+print_result:
+    mov rdi, buffer
+    call itoa
+
+    ; Print number
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, buffer
+    mov rdx, rbx
+    syscall
+
+    ; Print newline
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    mov rax, 60     ; sys_exit
+    xor rdi, rdi    ; return 0
+    syscall
+
+error:
+    mov rax, 60     ; sys_exit
+    mov rdi, 1      ; return 1
+    syscall
+
+; Convert string to integer
+atoi:
+    xor rax, rax
 .loop:
-    movzx rdx, byte [rdi + rcx]
-    test dl, dl         ; Check for end of string
+    movzx rdx, byte [rdi]
+    test dl, dl
     jz .done
-    
-    cmp dl, '0'
-    jl _error
-    cmp dl, '9'
-    jg _error
-    
     sub dl, '0'
+    cmp dl, 9
+    ja error
     imul rax, 10
     add rax, rdx
-    inc rcx
+    inc rdi
+    jmp .loop
+.done:
+    ret
+
+; Convert integer to string
+itoa:
+    mov rsi, buffer
+    add rsi, 31
+    mov byte [rsi], 0
+    mov rbx, 0      ; Length counter
+
+    test rax, rax
+    jnz .loop
+
+    dec rsi
+    mov byte [rsi], '0'
+    inc rbx
+    jmp .done
+
+.loop:
+    test rax, rax
+    jz .done
+
+    xor rdx, rdx
+    mov rcx, 10
+    div rcx
+    add dl, '0'
+    dec rsi
+    mov [rsi], dl
+    inc rbx
     jmp .loop
 
 .done:
-    test r10b, r10b     ; Check if number was negative
-    jz .return
-    neg rax             ; Make number negative
-
-.return:
+    mov rdi, buffer
+    mov rcx, rbx
+    rep movsb
     ret
-
-_all_same:
-    xor rdi, rdi        ; Return 0
-    jmp _exit
-
-_not_same:
-    mov rdi, 1          ; Return 1
-    jmp _exit
-
-_error:
-    mov rdi, 2          ; Return 2 for error
-
-_exit:
-    mov rax, 60         ; sys_exit
-    syscall
