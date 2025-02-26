64c3187548c79456b439330df6568eac2a1ff37e
mariam-rg <mariamregragui2002@gmail.com>
2025-02-27 00:11:43 +0100

    update

--- asm18/asm18.s
+++ asm18/asm18.s
@@ -2,28 +2,28 @@ section .data
     ; UDP Server details
     server_ip dd 0x0100007F       ; 127.0.0.1 in network byte order
     server_port dw 0x3905         ; port 1337 in network byte order
-    
+
     ; Message to send
     message db "Hello, server!", 0
     message_len equ $ - message
-    
+
     ; Timeout message
     timeout_msg db "Timeout: no response from server", 10
     timeout_msg_len equ $ - timeout_msg
-    
+
     ; Socket constants
     AF_INET equ 2
     SOCK_DGRAM equ 2
     SOL_SOCKET equ 1
     SO_RCVTIMEO equ 20
-    
+
 section .bss
     ; Buffer for received data
     recv_buffer resb 1024
-    
+
     ; Socket address structure for server
     server_addr resb 16
-    
+
     ; Timeout structure (timeval)
     timeout resb 16
 
@@ -37,23 +37,23 @@ _start:
     mov rsi, SOCK_DGRAM  ; Type: UDP
     xor rdx, rdx         ; Protocol: 0 (default)
     syscall
-    
+
     ; Check if socket creation was successful
     cmp rax, 0
     jl exit_error
-    
+
     ; Save socket descriptor
     mov r12, rax
-    
+
     ; Setup server address structure
     mov dword [server_addr], AF_INET      ; sa_family = AF_INET
     mov word [server_addr+2], [server_port] ; sin_port
     mov dword [server_addr+4], [server_ip]  ; sin_addr
-    
+
     ; Setup timeout structure (1 second)
     mov qword [timeout], 1      ; 1 second
     mov qword [timeout+8], 0    ; 0 microseconds
-    
+
     ; Set socket timeout option
     mov rax, 54                 ; sys_setsockopt
     mov rdi, r12                ; sockfd
@@ -62,11 +62,11 @@ _start:
     mov r10, timeout            ; optval
     mov r8, 16                  ; optlen
     syscall
-    
+
     ; Check if setting option was successful
     cmp rax, 0
     jl close_and_error
-    
+
     ; Send message to server
     mov rax, 44                 ; sys_sendto
     mov rdi, r12                ; sockfd
@@ -76,11 +76,11 @@ _start:
     mov r8, server_addr         ; dest_addr
     mov r9, 16                  ; addrlen
     syscall
-    
+
     ; Check if send was successful
     cmp rax, 0
     jl close_and_error
-    
+
     ; Receive response from server
     mov rax, 45                 ; sys_recvfrom
     mov rdi, r12                ; sockfd
@@ -90,21 +90,21 @@ _start:
     xor r8, r8                  ; src_addr (NULL)
     xor r9, r9                  ; addrlen (NULL)
     syscall
-    
+
     ; Check if receive was successful
     cmp rax, 0
     jle handle_timeout
-    
+
     ; Save bytes received
     mov r13, rax
-    
+
     ; Print received message
     mov rax, 1                  ; sys_write
     mov rdi, 1                  ; stdout
     mov rsi, recv_buffer        ; buf
     mov rdx, r13                ; count
     syscall
-    
+
     ; Print newline
     mov byte [recv_buffer], 10  ; newline character
     mov rax, 1                  ; sys_write
@@ -112,15 +112,15 @@ _start:
     mov rsi, recv_buffer        ; buf
     mov rdx, 1                  ; count
     syscall
-    
+
     ; Close socket and exit success
     mov rax, 3                  ; sys_close
     mov rdi, r12                ; fd
     syscall
-    
+
     xor rdi, rdi                ; Exit code 0
     jmp exit
-    
+
 handle_timeout:
     ; Print timeout message
     mov rax, 1                  ; sys_write
@@ -128,16 +128,16 @@ handle_timeout:
     mov rsi, timeout_msg        ; buf
     mov rdx, timeout_msg_len    ; count
     syscall
-    
+
 close_and_error:
     ; Close socket
     mov rax, 3                  ; sys_close
     mov rdi, r12                ; fd
     syscall
-    
+
 exit_error:
     mov rdi, 1                  ; Exit code 1
-    
+
 exit:
     mov rax, 60                 ; sys_exit
     syscall
