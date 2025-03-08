370b5b44925ca4a9bd135a4f1b665e1598009473
mariam-rg <mariamregragui2002@gmail.com>
2025-03-08 14:36:30 +0100

    update

--- asm15/asm15.s
+++ asm15/asm15.s
@@ -1,74 +1,74 @@
-section .bss
-    header: resb 16         ; Buffer for ELF header (first 16 bytes)
-
 section .data
-    elf_magic: db 0x7F, "ELF"  ; ELF magic numbers
-    x86_64_machine: equ 0x3E   ; Machine type for x86-64
+    elf_magic db 0x7f, "ELF"    ; ELF magic numbers
+
+section .bss
+    buffer resb 16              ; Buffer for ELF header
 
 section .text
     global _start
 
 _start:
-    ; Check if we have exactly one argument
-    pop rdi                 ; Get argc
-    cmp rdi, 2             ; Should be 2 (program name + filepath)
-    jne not_elf            ; If not 2 arguments, exit with error
-
-    ; Get filepath pointer (skip program name)
-    pop rdi                ; Skip program name
-    pop rdi                ; Get filepath pointer
+    ; Get argc and check arguments
+    pop rcx                     ; Get argc
+    cmp rcx, 2                  ; Check if we have exactly one argument
+    jne _not_elf_no_close
+    
+    pop rcx                     ; Skip program name
+    pop rsi                     ; Get file path into rsi
 
     ; Open file
-    mov rax, 2            ; sys_open
-    xor rsi, rsi          ; O_RDONLY
+    mov rax, 2                  ; sys_open
+    mov rdi, rsi                ; filename
+    xor rsi, rsi                ; O_RDONLY
     syscall
-
-    ; Check if file opened successfully
-    cmp rax, 0
-    jl not_elf            ; Exit if error (negative value)
-
-    ; Save file descriptor
-    mov r8, rax           ; Store fd in r8
-
-    ; Read first 16 bytes (ELF header)
-    mov rax, 0            ; sys_read
-    mov rdi, r8           ; fd
-    mov rsi, header       ; buffer
-    mov rdx, 16           ; count
+    
+    ; Check if open succeeded
+    cmp rax, 0                  
+    jl _not_elf_no_close
+    mov rdi, rax                ; Save file descriptor in rdi for read
+    
+    ; Read file header
+    mov rax, 0                  ; sys_read
+    mov rsi, buffer             ; buffer
+    mov rdx, 16                 ; count
     syscall
+    
+    ; Save file descriptor for later close
+    push rdi
+    
+    ; Check if read succeeded with 16 bytes
+    cmp rax, 16                 
+    jne _not_elf
 
-    ; Check if read was successful
-    cmp rax, 16
-    jne close_and_not_elf ; If couldn't read 16 bytes, not an ELF
-
-    ; Check ELF magic numbers (0x7F 'E' 'L' 'F')
-    mov rsi, header
-    mov rdi, elf_magic
-    mov rcx, 4            ; Compare 4 bytes
+    ; Check ELF magic numbers
+    mov rsi, buffer             ; Source for comparison
+    mov rdi, elf_magic          ; Target for comparison
+    cld                         ; Clear direction flag
+    mov rcx, 4                  ; Compare 4 bytes
     repe cmpsb
-    jne close_and_not_elf ; If not equal, not an ELF
+    jne _not_elf
 
-    ; Check machine type (x86-64 = 0x3E at offset 18)
-    mov al, byte [header + 0x12]  ; e_machine field
-    cmp al, x86_64_machine
-    jne close_and_not_elf
+    ; Check machine type (x86-64 = 0x3E)
+    cmp byte [buffer + 0x12], 0x3E
+    jne _not_elf
 
-    ; Close file and exit success
-    mov rax, 3            ; sys_close
-    mov rdi, r8           ; fd
+    ; Success - is ELF x86-64
+    pop rdi                     ; Restore file descriptor
+    mov rax, 3                  ; sys_close
     syscall
-    xor rdi, rdi          ; Exit code 0
-    jmp exit
+    xor rdi, rdi                ; Exit code 0
+    jmp _exit
 
-close_and_not_elf:
-    ; Close file
-    mov rax, 3            ; sys_close
-    mov rdi, r8           ; fd
+_not_elf:
+    pop rdi                     ; Restore file descriptor
+    mov rax, 3                  ; sys_close
     syscall
+    mov rdi, 1                  ; Exit code 1
+    jmp _exit
 
-not_elf:
-    mov rdi, 1            ; Exit code 1
+_not_elf_no_close:
+    mov rdi, 1                  ; Exit code 1
 
-exit:
-    mov rax, 60           ; sys_exit
+_exit:
+    mov rax, 60                 ; sys_exit
     syscall
